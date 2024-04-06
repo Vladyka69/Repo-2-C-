a06a96f49bda297bb64e7bc91a3451a89eeef140
PIBO0122010 Котов Сергей Владимирович <PIBO0122010@sibupk.su>
2024-04-06 19:38:26 +0700

    first commit

---
+++ Repo1/Repo1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+bool isLeapYear(int year) {
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            return true;
+        }
+    }
+    else {
+        return false;
+    }
+}
+
+int main(){
+    setlocale(0, "rus");
+    int year;
+    cout << "Введите год: ";
+    cin >> year;
+
+    if (isLeapYear(year)) {
+        cout << year << " год високосный" << endl;
+    }
+    else {
+        cout << year << " год не високосный" << endl;
+    }
+
+    return 0;
+}
